462f47ff258e82d2e1b168cbe3899ac86028e521
ndavisontest <58317833+ndavisontest@users.noreply.github.com>
2023-07-30 18:05:53 +1000

    Update build.ps1

--- tracer/build.ps1
+++ tracer/build.ps1
@@ -1,1 +1,7 @@
-"hello there"
+[CmdletBinding()]
+Param(
+    [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
+    [string[]]$BuildArguments
+)
+
+Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
